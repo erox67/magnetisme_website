dcf5cd03dcf042a85a75d170f1173ed37607a1fb
erox67 <eroxcontact@gmail.com>
2023-09-02 12:01:14 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'vert': '##71A376',
+      'rose': '#F5E0E5',
+      'jaune': '#F4DD90',
+      'vert-bleu': '#DBF9F4',
+      'gris': '#D2D2DA',
+      },
+    extend: {},
+  },
+  plugins: [],
+}
+
